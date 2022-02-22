8f66606063c9ecdf7d6ecad736b6cb8cd6e995e6
Ewelina <ekowalczyk1994@gmail.com>
2022-02-22 14:20:05 -0600

    user, expense, categories models were created together with associated migrations

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,5 @@
 class NewsController < ApplicationController
 
-    #require 'rest-client'
-
     def get_news
         url = "https://finnhub.io/api/v1/news?category=general&token=#{ENV["KEY"]}"
         response = RestClient.get(url)
