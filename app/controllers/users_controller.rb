db01f8cbac6c72f1844fc996a469c78d79a37db8
Ewelina Kowalczyk <89812100+Ewelajna14@users.noreply.github.com>
2022-03-12 16:09:20 -0600

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,7 @@ class UsersController < ApplicationController
         render json: @current_user
     end
 
-    def index
-        users = User.all
-        render json: users, each_serializer: UserSerializer
-    end
-
+    
 
 
     private
