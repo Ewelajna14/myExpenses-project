955ffc037326bde3e3e3eccff359a0886e6d95f4
Ewelina Kowalczyk <89812100+Ewelajna14@users.noreply.github.com>
2022-06-17 09:21:25 -0500

    Update news_controller.rb

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,7 +1,7 @@
 class NewsController < ApplicationController
 
     def get_news
-        url = "https://finnhub.io/api/v1/news?category=general&token=#{ENV["KEY"]}"
+         url = "https://finnhub.io/api/v1/news?category=general&token=#{ENV["MY_API_KEY"]}"
         response = RestClient.get(url)
         render json: response
     end
